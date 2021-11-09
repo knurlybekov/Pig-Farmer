12ada942932bf3d38ae1d388649106529280103e
Ompadhyay9 <90451128+Ompadhyay9@users.noreply.github.com>
2021-11-09 10:16:04 +0530

    Update Feed.java

--- Feed.java
+++ Feed.java
@@ -1,1 +1,29 @@
+package com.company;
 
+public class Feed {
+    private static int binCount;
+
+    private final String feedName;
+
+    public static int getAmount() {
+        return binCount;
+    }
+
+    public String getFeedName() {
+        return feedName;
+    }
+
+
+    public static void Consume() {
+        if (binCount > 0) {
+            binCount -= 3;
+        }else{
+            System.out.println("There is no food bins left to feed.");
+        }
+    }
+    Feed(String feedName_f, int binCount_b) {
+        feedName = feedName_f;
+        binCount = binCount + binCount_b;
+    }
+
+}
