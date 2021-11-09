6c88a1363fe863b2a364a6ee1f98b71935c6ac52
Nikolay Sekerbayev <90434924+KolyaXGod@users.noreply.github.com>
2021-11-08 20:46:34 -0800

    Pig.java
    Have a good day professor

--- Pig.java
+++ Pig.java
@@ -1,1 +1,44 @@
+package com.company;
+import java.util.Date;
 
+public class Pig {
+    private static int track = 0;
+
+    private boolean isFemale;
+    private String name, color, sex;
+    private String dateOfBirth;
+
+    public static int getCount() {
+        return track;
+    }
+
+    //getter
+    public String getName() {
+        return name;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public boolean isFemale() {
+        return false;
+    }
+    Pig(String name_n, String color_c, String sex_s, String dateOfBirth_d) {
+        name = name_n;
+        sex = sex_s;
+        color = color_c;
+        dateOfBirth = dateOfBirth_d;
+    }
+    public void feed(Feed foodNo) {
+        foodNo.Consume();
+    }
+}
