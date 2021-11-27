10037aa66fd787561ba6788a4069a3e0fac0e547
knurlybekov <75376694+knurlybekov@users.noreply.github.com>
2021-11-26 18:51:27 -0800

    Update Farmer.java

--- Farmer.java
+++ Farmer.java
@@ -1,76 +1,83 @@
 package com.company;
 import java.time.LocalDate;
 import java.util.Scanner;
-import java.util.Date;
 
 public class Farmer {
+    static PigCollection pc = new PigCollection();
+    static FeedCollection fc = new FeedCollection();
 
     public static void main(String[] args) {
-        LocalDate localdate = LocalDate.now();
-        boolean run = true;
-        String db = localdate.toString();
-        //Create two different types of feed: apple and potato
-        int appleBins = 0;
-        int potatoBins = 0;
-        Pig pig1 = new Pig("Baha", "Brown", "male", db);//YY,MM,DD
-        Pig pig2 = new Pig("Yera", "White", "male", db);
-        Pig pig3 = new Pig("Maha", "Black", "Female", db);
         Scanner scan = new Scanner(System.in);
-        String feed;
-        String choice = "";
-        do {
-            System.out.println("1.Info about pigs");
-            System.out.println("2.Feed info and feed pigs");
-            System.out.println("TO EXIT PRESS TWICE <ENTER>");
-            choice = scan.nextLine();
+        LocalDate localdate = LocalDate.now();
+        String db = localdate.toString();
+        int pigid = 0;
+
+        System.out.println("Welcome to FARMER program!");
+        System.out.println("1.Add Pig");
+        System.out.println("2.Add Feed");
+        System.out.println("3.FeedPigs");
+        System.out.println("4.Report");
+        System.out.println("5.EXIT");
+        int choice = scan.nextInt();
+        while(choice >=1 && choice <= 5) {
             switch (choice) {
-                case "1":
-                    System.out.println("1st pig name is " + pig1.getName() + "\n gender: " + pig1.getSex() + "\n pig color: " + pig1.getColor() + "\n Date of Birth: " + pig1.getDateOfBirth());
-                    System.out.println();
-                    System.out.println("2nd pig name is " + pig2.getName() + "\n gender: " + pig2.getSex() + "\n pig color: " + pig2.getColor() + "\n Date of Birth: " + pig2.getDateOfBirth());
-                    System.out.println();
-                    System.out.println("3rd pig name is " + pig3.getName() + "\n gender: " + pig3.getSex() + "\n pig color: " + pig3.getColor() + "\n Date of Birth: " + pig3.getDateOfBirth());
-                    System.out.println();
-                    System.out.println("Total pigs: "+Pig.getCount());
-                    System.out.println();
+                case 1:
+                    System.out.println("Let`s create a pig");
+                    pigid++;
+                    System.out.println("What`s pig name?");
+                    String pname = scan.next();
+                    System.out.println("What`s pig sex? Male/Female");
+                    String psex = scan.next();
+                    System.out.println("What`s pig color?");
+                    String pcolor = scan.next();
+                    System.out.println("You CREATED a pig.");
+                    Pig pig = new Pig(pname, psex, pcolor, db);
+                    pc.addPig(pig);
+                    System.out.println("ID: " + pigid + "\tName: " + pname + "\tSex: "
+                            + psex + "\tColor: " + pcolor + "\tDate of Birth: " + db);
+                    choice = scan.nextInt();
                     break;
-                case "2":
-                    System.out.println("How many new bins of Apple do you have?");
-                    appleBins = scan.nextInt();
-                    Feed feed1 = new Feed("Apple", appleBins);
-                    System.out.println("How many new bins of Potato do you have?");
-                    potatoBins = scan.nextInt();
-                    Feed feed2 = new Feed("Potato", potatoBins);
-                    System.out.println("Do you want to feed the pigs?(Y/N)");
-                    String fdpg;
-                    fdpg = scan.next();
-                    switch (fdpg) {
-                        case "Y":
-                            Feed.Consume();
-                            System.out.println(pig1.getName() + ", " + pig2.getName() + " and " + pig3.getName() + " ate " + feed1.getFeedName() + " and " + feed2.getFeedName());
-                            System.out.println("Total left: " + Feed.getAmount() + " bins");
-                            System.out.println("Would you like to go again?? \nY/N");
-                            fdpg = scan.next();
-                            switch (fdpg) 
-                            {
-                                case "Y":
-                                    Feed.Consume();
-                                    System.out.println(pig1.getName() + ", " + pig2.getName() + " and " + pig3.getName() + " ate " + feed1.getFeedName() + " and " + feed2.getFeedName());
-                                    System.out.println("Total left: " + Feed.getAmount() + " bins");
-                                    System.out.println("Would you like to go again?? \nY/N");
-                                    fdpg = scan.next();
-                                    break;
-                                case "N":
-                                    System.out.println("The pigs will not be fed.");
-                                    System.out.println("Total left: " + Feed.getAmount() + " bins");
-                                    break;
-                             }
-                                case "N":
-                                    System.out.println("The pigs will not be fed.");
-                                    System.out.println("Total left: " + Feed.getAmount() + " bins");
+                case 2:
+                    System.out.println("Let`s create a feed");
+                    System.out.println("How many number of bin do yoy want?");
+                    int nb = scan.nextInt();
+                    System.out.println("Feed type: ");
+                    String ft = scan.nextLine();
+                    System.out.println("Weight (in kg): ");
+                    double wkg = scan.nextDouble();
+                    Feed newFeed = new Feed(ft);
+                    fc.setFeedlocation(nb, newFeed);
+                    System.out.println("Feed has been added at " + nb);
+                    choice = scan.nextInt();
+                    break;
+
+                case 3:
+                    if (pc.isEmpty()){
+                        System.out.println("You do not have any pigs. Please add them.");
+                    } else {
+                        for (int i = 0; i < pc.getPigCount(); i++) {
+                            Feed tempf = fc.getNextFullBin();
+                            System.out.println(fc.getNumberOfFullBins() + " bin(s) available.");
+                            if (tempf != null) {
+                                pc.getPigArray()[i].feed(tempf);
+                                System.out.println("Bin used!");
+                            } else {
+                                System.out.println("No more bins are left! Go back and add one...");
                                 break;
                             }
+                            break;
                         }
-        }while (!choice.isEmpty());
+                    }
+                case 4:
+                    System.out.println("Bins avalible: " + fc.getSize());
+                    String pigList = pc.getPigList();
+                    System.out.println("Total pigs: "+pc.getPigCount());
+                    System.out.println("Pigs: " + pigList);
+                    choice = scan.nextInt();
+                    break;
+                case 5:
+                    System.exit(0);
+
+            }
     }
-}
+}}
